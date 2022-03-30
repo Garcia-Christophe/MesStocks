8b0eec12c707a02e099109422686b8892cb4d227
Garcia-Christophe <garciachristophe29@gmail.com>
2022-03-30 22:03:57 +0200

    import QRCode

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,41 +1,71 @@
-import React from "react";
-import { StyleSheet, View, Text, Alert, TouchableOpacity } from "react-native";
-
-//import QRCodeScanner from "react-native-qrcode-scanner";
-//import { RNCamera } from "react-native-camera";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const Scan = () => {
+  const [permissionOK, setPermissionOK] = useState(null);
+  const [articleScanne, setArticleScanne] = useState(false);
+  const [contenu, setContenu] = useState("vide");
+
+  // Demander la permission de la caméra
+  const demanderPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setPermissionOK(status == "granted");
+      alert(status == "granted");
+    })();
+  };
+
+  // Demander la permission de la caméra dès qu'on arrive sur la page
+  useEffect(() => {
+    demanderPermission();
+  }, []);
+
+  // Lorsqu'un qrcode/barcode est scanné (type = qrcode/barcode, data = contenu)
+  const handleScan = ({ type, data }) => {
+    setArticleScanne(true);
+    setContenu(data);
+    console.log("type: " + type + "\nData: " + data);
+  };
+
+  // Vérification des permissions, et return en conséquence
+  if (permissionOK === null) {
+    return (
+      <View style={styles.container}>
+        <Text>L'application a besoin de votre permission.</Text>
+      </View>
+    );
+  }
+
+  if (permissionOK === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>Pas d'accès à la caméra.</Text>
+        <Button
+          title="Autoriser la caméra"
+          onPress={() => demanderPermission}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      {/* <QRCodeScanner
-        onRead={(e) => Alert.alert("ID Trouvé !", "ID : " + e.data)}
-        topContent={
-          <Text>
-            Scanne le QR Code de l'article dont tu souhaites ajuster le stock.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity>
-            <Text>OK. Compris !</Text>
-          </TouchableOpacity>
-        }
-      /> */}
-      {/* <RNCamera
-        ref={(ref) => console.log(ref)}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        captureAudio={false}
-        androidCameraPermissionOptions={{
-          title: "Permission to use camera",
-          message: "We need your permission to use your camera",
-          buttonPositive: "Ok",
-          buttonNegative: "Cancel",
-        }}
-        onGoogleVisionBarcodesDetected={({ barcodes }) => {
-          console.log(barcodes);
-        }}
-      /> */}
+      <View style={styles.qrcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={articleScanne ? undefined : handleScan}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.texteContenu}>{contenu}</Text>
+
+      {articleScanne && (
+        <Button
+          title="Scanner"
+          onPress={() => setArticleScanne(false)}
+          color="black"
+        />
+      )}
     </View>
   );
 };
@@ -61,6 +91,19 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
 
     elevation: 8,
+  },
+  qrcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "white",
+  },
+  texteContenu: {
+    fontSize: 16,
+    margin: 20,
   },
 });
 
